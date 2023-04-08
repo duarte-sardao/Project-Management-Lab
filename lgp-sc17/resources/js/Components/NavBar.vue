1417852284724c05d06228b0ca840b064a1a7577
fernandorego <fernandoluisrego@gmail.com>
2023-04-08 10:39:21 +0100

    Fix form in navbars

--- lgp-sc17/resources/js/Components/NavBar.vue
+++ lgp-sc17/resources/js/Components/NavBar.vue
@@ -1,4 +1,7 @@
 <script setup>
+import {Link, useForm} from "@inertiajs/vue3";
+import TextInput from "@/Components/TextInput.vue";
+
 const props = defineProps({
     title: {
         default: "Page Title",
@@ -7,6 +10,16 @@ const props = defineProps({
         default: null
     }
 });
+
+const form = useForm({
+    search: '',
+});
+
+const submit = () => {
+    /* form.post(route('search'), {
+        onFinish: () => {},
+    }); */
+};
 </script>
 
 <template>
@@ -19,15 +32,23 @@ const props = defineProps({
                 </div>
             </label>
             <div class="flex-1 ml-7">
-                <a class="btn btn-ghost text-gray-600 normal-case text-xl" href="/">Home</a>
-                <a class="btn btn-ghost text-gray-600 normal-case text-xl" href="/library">Library</a>
-                <a class="btn btn-ghost text-gray-600 normal-case text-xl" href="/about">About Us</a>
+                <Link class="btn btn-ghost text-gray-600 normal-case text-xl" :href="route('homepage')">Home</Link>
+                <Link class="btn btn-ghost text-gray-600 normal-case text-xl" :href="route('library')">Library</Link>
+                <Link class="btn btn-ghost text-gray-600 normal-case text-xl" :href="route('about')">About Us</Link>
             </div>
             <div class="flex-none gap-2">
-                <a class="btn bg-[#B9CEED] hover:bg-[#578AD6] hover:text-gray-200 text-gray-600 border-0 normal-case text-xl rounded-full px-7 mr-7">Log in</a>
-                <form>
+                <Link :href="route('login')" class="btn bg-[#B9CEED] hover:bg-[#578AD6] hover:text-gray-200 text-gray-600 border-0 normal-case text-xl rounded-full px-7 mr-7">Log in</Link>
+                <form @submit.prevent="submit">
                     <div class="relative mr-7">
-                        <input type="search" id="search" class="input block w-full text-sm text-gray-900 input-bordered border-[#578AD6] rounded-full bg-gray-50" placeholder="Search" required>
+                        <TextInput
+                            id="search"
+                            type="search"
+                            class="input block w-full text-sm text-gray-900 input-bordered border-[#578AD6] rounded-full bg-gray-50"
+                            v-model="form.search"
+                            placeholder="Search"
+                            required
+                            autocomplete="search"
+                        />
                         <button type="submit" class="text-white absolute right-2.5 bottom-3.5 font-medium "><svg aria-hidden="true" class="w-5 h-5 text-[#578AD6]" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
                     </div>
                 </form>
