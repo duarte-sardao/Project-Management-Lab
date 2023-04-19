b0a597d0b8274cf336632b9c347c02e0bfc6e9af
Dudugs <duartesardao@sapo.pt>
2023-04-19 15:41:10 +0100

    trying to work on email but extremely confused

---
+++ lgp-sc17/app/Http/Controllers/MailController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MailerController extends AbstractController
+{
+    public function sendEmail(MailerInterface $mailer, Request $request)
+    {
+
+         $this->validate($request, [
+            'name' => ['required', 'string', 'max:255' ], 
+            'email' => ['required', 'string', 'email', 'max:255' ],
+            'subject' => ['required', 'string', 'max:255'],
+            'message' => ['required', 'string', 'max:255']
+      ]);
+
+
+        $email = (new Email())
+            ->from($request['email'])
+            ->to('healthybyte.noreply@gmail.com')
+            ->subject($request['subject'])
+            ->text($request['subject'])
+
+        $mailer->send($email);
+
+    }
+}
