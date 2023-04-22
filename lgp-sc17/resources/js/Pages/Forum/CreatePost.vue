ecf91fafbf7d2af457219960489b9151a7dd2390
Tiago Silva <silvatigas@hotmail.com>
2023-04-22 16:41:48 +0100

    Limit the number of topics associated with a post

--- lgp-sc17/resources/js/Pages/Forum/CreatePost.vue
+++ lgp-sc17/resources/js/Pages/Forum/CreatePost.vue
@@ -1,12 +1,24 @@
 <script setup>
+import { ref } from 'vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 import NavBarSimple from "@/Components/NavBarSimple.vue";
 import Footer from "@/Components/Footer.vue";
 import TopicButton from "@/Components/TopicButton.vue";
+import InputError from "@/Components/InputError.vue";
+
+const topicsSelected = ref(0);
+const topicsError = ref('');
 
 const clickTopic = (index) => {
-    topics[index].selected = !topics[index].selected;
-};
+    const selected = topics[index].selected;
+    if (topicsSelected.value < 4 || selected) {
+        topicsSelected.value += (selected) ? -1 : 1;
+        topics[index].selected = !topics[index].selected;
+        if (topicsError.value) topicsError.value = '';
+    } else if (!topicsError.value) {
+        topicsError.value = 'A post can only have up to 4 topics associated. Please remove any additional topics before trying to add a new one.';
+    }
+}
 
 defineProps({
     canResetPassword: Boolean,
@@ -17,8 +29,6 @@ const form = useForm({
     credential: '',
     password: '',
 });
-
-
 
 // const submit = () => {
 //     form.post(route('login'), {
@@ -60,11 +70,14 @@ const topics = [
                 class="resize-none border-[#E9EFFD] w-[100%] h-[25vh] mt-[3vh] rounded-3xl py-2 px-4 text-lg text-black"
                 placeholder="Add text"
             />
-            <div class="mt-[3vh] pl-[2vw]">
-                <div class="text-[#6D6D6D] text-lg">Choose one or more topics</div>
+            <div class="mt-[3vh] pl-[2vw] h-[30vh]">
+                <div :key="topicsSelected" class="text-[#6D6D6D] text-lg">
+                    Choose one or more topics ({{topicsSelected}}/4)
+                </div>
                 <div id="topics-area" class="max-h-[25vh] overflow-auto">
                     <TopicButton v-for="(topic, index) in topics" :topic="topic" :index="index" v-on:click="clickTopic(index)"/>
                 </div>
+                <InputError :message="topicsError" />
             </div>
             <button class="shadow-md shadow-black/[.25] justify-self-end mt-[3vh] mb-[10vh] bg-[#578AD6] px-20 py-3 text-xl text-white font-bold rounded-3xl ">
                 Post
