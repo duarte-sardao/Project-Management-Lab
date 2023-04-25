5ace764456ad4c3635659dbd32f4ac7070a84932
Fernando Rego <64474631+fernandorego@users.noreply.github.com>
2023-04-26 00:14:06 +0100

    Remove search input in the navbar (#34)

--- lgp-sc17/resources/js/Components/NavBarContentCollapsed.vue
+++ lgp-sc17/resources/js/Components/NavBarContentCollapsed.vue
@@ -1,21 +1,10 @@
 <script setup>
-import {Link, useForm, usePage} from "@inertiajs/vue3";
-import TextInput from "@/Components/TextInput.vue";
+import {Link, usePage} from "@inertiajs/vue3";
 
 let profile_img_url = '/svg_icons/profile.svg';
 if (usePage().props.auth.user && usePage().props.auth.user.profile_img_url) {
     profile_img_url = usePage().props.auth.user.profile_img_url;
 }
-
-const form = useForm({
-    search: '',
-});
-
-const submit = () => {
-    /* form.post(route('search'), {
-        onFinish: () => {},
-    }); */
-};
 </script>
 
 <template>
@@ -39,22 +28,6 @@ const submit = () => {
                     <Link v-else :href="route('profile')" class="btn p-0 border-0 normal-case text-xl rounded-full bg-transparent hover:bg-lightBlue">
                         <img id="profile-img" class="rounded-full mx-auto h-fit w-[48px] h-[48px]" :src="profile_img_url" alt="profile image">
                     </Link>
-                </li>
-                <li>
-                    <form class="p-0 my-2" @submit.prevent="submit">
-                        <div class="relative">
-                            <TextInput
-                                id="search"
-                                type="search"
-                                class="input block w-full text-sm text-gray-900 input-bordered border-mainBlue rounded-full"
-                                v-model="form.search"
-                                placeholder="Search"
-                                required
-                                autocomplete="search"
-                            />
-                            <button type="submit" class="text-white absolute right-2.5 bottom-3.5 font-medium "><svg aria-hidden="true" class="w-5 h-5 text-mainBlue" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
-                        </div>
-                    </form>
                 </li>
             </ul>
         </div>
