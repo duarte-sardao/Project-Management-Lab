cde6f5420226bc746dc0657dfdaffa1340240e7e
Tiago Silva <silvatigas@hotmail.com>
2023-04-27 14:15:23 +0100

    Create migrations

---
+++ lgp-sc17/database/migrations/2023_04_25_115724_create_answers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->unsignedBigInteger('forum_post_id');
+            $table->foreign('forum_post_id')->references('id')->on('forum_posts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
