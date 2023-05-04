b49e48275bae84e21ca25027df014e6e30c46d83
fernandorego <fernandoluisrego@gmail.com>
2023-05-04 15:16:50 +0100

    Finish library page

---
+++ lgp-sc17/resources/js/Components/Common/PostCard.vue
@@ -0,0 +1,60 @@
+<script setup>
+const props = defineProps({
+    'is_text_top': {
+        type: Boolean,
+        required: true,
+    },
+    'img_url': {
+        type: String,
+        default: '/svg_img/default-post.jpg'
+    }
+});
+
+</script>
+
+<template>
+    <div v-if="is_text_top" class="card bg-base-100 rounded-3xl bg-white">
+        <div id="post" class="bg-cover bg-center h-[24rem] w-full rounded-[30px]" :style="'background-image: url(' + img_url + ')'">
+            <div class="card-body p-3 pt-10">
+                <div class="card bg-base-100 rounded-3xl h-32 bg-opacity-20 backdrop-blur-md shadow-lg grid grid-cols-[70%_30%] bg-white">
+                    <div>
+                        <h2 class="card-title pl-4 pt-3 font-bold text-2xl ">Shoes!</h2>
+                        <p class="pl-4">If a dog chews shoes whose shoes does he choose?</p>
+                    </div>
+                    <div class="card-actions items-end pb-3 pl-2">
+                        <button class="btn btn-circle bg-white hover:bg-mainBlue text-gray-400 hover:text-white text-xl font-thin border-none">❯</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div v-else class="card bg-base-100 rounded-3xl bg-white">
+        <div id="post" class="bg-cover bg-center h-[24rem] w-full rounded-[30px]" :style="'background-image: url(' + img_url + ')'">
+            <div class="card-body p-3 absolute inset-x-0 bottom-10">
+                <div class="card bg-base-100 rounded-3xl h-32 bg-opacity-20 backdrop-blur-md shadow-lg grid grid-cols-[70%_30%]">
+                    <div>
+                        <h2 class="card-title pl-4 pt-3 font-bold text-2xl dark:text-black">Shoes!</h2>
+                        <p class="pl-4">If a dog chews shoes whose shoes does he choose?</p>
+                    </div>
+                    <div class="card-actions items-end pb-3 pl-2">
+                        <button class="btn btn-circle bg-white hover:bg-mainBlue text-gray-400 hover:text-white text-xl font-thin border-none">❯</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostCard"
+}
+</script>
+
+<style scoped>
+@media all and (max-width: 1300px) {
+    #post {
+        max-height: 12rem;
+    }
+}
+</style>
--- lgp-sc17/resources/js/Pages/Library.vue
+++ lgp-sc17/resources/js/Pages/Library.vue
@@ -1,7 +1,8 @@
 <script setup>
 import NavBar from "@/Components/NavBar.vue";
 import Footer from "@/Components/Footer.vue";
-import LibrarySearch from "@/Components/LibrarySearch.vue";
+import LibrarySearch from "@/Components/Library/LibrarySearch.vue";
+import PostCard from "@/Components/Common/PostCard.vue";
 
 const title="Library"
 const subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt ligula aliquet"
@@ -16,67 +17,11 @@ const subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
     <LibrarySearch></LibrarySearch>
 
     <div class="px-[10%] pb-16 pt-5">
-        <div class="grid grid-cols-4 gap-6 h-[28rem]">
-            <div class="card bg-base-100 rounded-3xl bg-white">
-                <div class="bg-cover bg-center h-[26rem] w-full rounded-[30px]" style="background-image: url('/svg_img/carr1.svg');">
-                    <div class="card-body p-3 absolute inset-x-0 bottom-10">
-                        <div class="card bg-base-100 rounded-3xl h-32 bg-opacity-20 backdrop-blur-md shadow-lg grid grid-cols-[70%_30%]">
-                            <div>
-                                <h2 class="card-title pl-4 pt-3 font-bold text-2xl dark:text-black">Shoes!</h2>
-                                <p class="pl-4">If a dog chews shoes whose shoes does he choose?</p>
-                            </div>
-                            <div class="card-actions items-end pb-3 pl-2">
-                                <button class="btn btn-circle bg-white hover:bg-mainBlue text-gray-400 hover:text-white text-xl font-thin border-none">❯</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card bg-base-100 rounded-3xl bg-white">
-                <div class="bg-cover bg-center h-[26rem] w-full rounded-[30px]" style="background-image: url('/svg_img/carr2.svg');">
-                    <div class="card-body p-3 pt-10">
-                        <div class="card bg-base-100 rounded-3xl h-32 bg-opacity-20 backdrop-blur-md shadow-lg grid grid-cols-[70%_30%] bg-white">
-                            <div>
-                                <h2 class="card-title pl-4 pt-3 font-bold text-2xl ">Shoes!</h2>
-                                <p class="pl-4">If a dog chews shoes whose shoes does he choose?</p>
-                            </div>
-                            <div class="card-actions items-end pb-3 pl-2">
-                                <button class="btn btn-circle bg-white hover:bg-mainBlue text-gray-400 hover:text-white text-xl font-thin border-none">❯</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card bg-base-100 rounded-3xl bg-white">
-                <div class="bg-cover bg-center h-[26rem] w-full rounded-[30px]" style="background-image: url('/svg_img/carr3.svg');">
-                    <div class="card-body p-3 absolute inset-x-0 bottom-10">
-                        <div class="card bg-base-100 rounded-3xl h-32 bg-opacity-20 backdrop-blur-md shadow-lg grid grid-cols-[70%_30%] bg-white">
-                            <div>
-                                <h2 class="card-title pl-4 pt-3 font-bold text-2xl ">Shoes!</h2>
-                                <p class="pl-4">If a dog chews shoes whose shoes does he choose?</p>
-                            </div>
-                            <div class="card-actions items-end pb-3 pl-2">
-                                <button class="btn btn-circle bg-white hover:bg-mainBlue text-gray-400 hover:text-white text-xl font-thin border-none">❯</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card bg-base-100 rounded-3xl bg-white">
-                <div class="bg-cover bg-center h-[26rem] w-full rounded-[30px]" style="background-image: url('/svg_img/carr4.svg');">
-                    <div class="card-body p-3 pt-10">
-                        <div class="card bg-base-100 rounded-3xl h-32 bg-opacity-20 backdrop-blur-md shadow-lg grid grid-cols-[70%_30%] bg-white">
-                            <div>
-                                <h2 class="card-title pl-4 pt-3 font-bold text-2xl ">Shoes!</h2>
-                                <p class="pl-4">If a dog chews shoes whose shoes does he choose?</p>
-                            </div>
-                            <div class="card-actions items-end pb-3 pl-2">
-                                <button class="btn btn-circle bg-white hover:bg-mainBlue text-gray-400 hover:text-white text-xl font-thin border-none">❯</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div id="posts" class="grid grid-cols-4 gap-6">
+            <PostCard :is_text_top="false"></PostCard>
+            <PostCard :is_text_top="true"></PostCard>
+            <PostCard :is_text_top="false"></PostCard>
+            <PostCard :is_text_top="true"></PostCard>
         </div>
     </div>
 
@@ -90,5 +35,15 @@ export default {
 </script>
 
 <style scoped>
+@media all and (max-width: 1300px) {
+    #posts {
+        grid-template-columns: repeat(2, minmax(0, 1fr));
+    }
+}
 
+@media all and (max-width: 750px) {
+    #posts {
+        grid-template-columns: repeat(1, minmax(0, 1fr));
+    }
+}
 </style>
