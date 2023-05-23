d95ba6183316bc96ab6d82818f7950ac941414f1
fernandorego <fernandoluisrego@gmail.com>
2023-05-23 16:46:14 +0100

    Add checks to hospital deletion

--- lgp-sc17/app/Http/Controllers/HospitalController.php
+++ lgp-sc17/app/Http/Controllers/HospitalController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\HospitalPostUpdateRequest;
 use App\Models\Hospital;
+use App\Models\Medic;
+use App\Models\Patient;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -20,8 +23,21 @@ class HospitalController extends Controller
     }
 
     function delete($id, Request $request) {
-        $post = Hospital::find($id);
-        $post->delete();
+        $hospital = Hospital::find($id);
+        $users = User::all();
+        foreach ($users as $user) {
+            if ($user->isPatient()) {
+                if (Patient::where('user_id','=',$user->id)->first()->hospital->id == $id) {
+                    return to_route('admin.hospitals')->with(['error' => 'hospitalDeletedError']);
+                }
+            } else if ($user->isMedic()) {
+                if (Medic::where('user_id','=',$user->id)->first()->hospital->id === $id) {
+                    return to_route('admin.hospitals')->with(['error' => 'hospitalDeletedError']);
+                }
+            }
+        }
+
+        $hospital->delete();
         return to_route('admin.hospitals')->with(['success' => 'hospitalDeleted']);
     }
 }
