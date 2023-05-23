d0951214d3e28dd6183196a2417a70ac7c356fb9
fernandorego <fernandoluisrego@gmail.com>
2023-05-23 16:49:30 +0100

    Fix typo

--- lgp-sc17/resources/js/Pages/Admin/Forum/Forum.vue
+++ lgp-sc17/resources/js/Pages/Admin/Forum/Forum.vue
@@ -98,7 +98,7 @@ const getResults = async (page = 1) => {
 </script>
 
 <template>
-    <Head><title>{{ $t('forumAdmnistrationTitle') }}</title></Head>
+    <Head><title>{{ $t('forumAdministrationTitle') }}</title></Head>
     <AdministrationLayout page="forum">
         <MessageToast
             v-if="displayToast"
