077d6e5204734cf7b008407d8f236fab3b58c6cd
fernandorego <fernandoluisrego@gmail.com>
2023-05-25 02:07:00 +0100

    Minor fix

--- lgp-sc17/app/Http/Controllers/ChatController.php
+++ lgp-sc17/app/Http/Controllers/ChatController.php
@@ -10,12 +10,16 @@ use Inertia\Inertia;
 class ChatController extends Controller
 {
     function chatMedics($patient_id, Request $request) {
+        $patients = Patient::join('users', 'users.id', '=', 'patients.user_id')
+            ->orderBy('patients.created_at', 'desc')
+            ->paginate(6);
+
         $medics = Medic::join('users', 'users.id', '=', 'medics.user_id')
             ->orderBy('medics.created_at', 'desc')
             ->paginate(6);
 
         return Inertia::render('Admin/Chat', [
-            'patients' => Patient::join('users', 'users.id', '=', 'patients.user_id')->orderBy('patients.created_at', 'desc')->paginate(6),
+            'patients' => $patients,
             'medics' => $medics
         ]);
     }
