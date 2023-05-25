1c6cdf9d3092b722ff62481988a4fe7bec762d64
Tiago Silva <silvatigas@hotmail.com>
2023-05-25 23:34:58 +0100

    Fix the size of the posts' topic tags on the forum page

--- lgp-sc17/resources/js/Components/ForumPost.vue
+++ lgp-sc17/resources/js/Components/ForumPost.vue
@@ -57,9 +57,13 @@ if (author == null) author = '/svg_icons/profile.svg';
                     </div>
                 </div>
             </div>
-            <div class="grid grid-flow-col gap-0 justify-end mr-[2vw]">
-                <div v-for="topic in data.topics" class="inline-block px-4 py-1 mr-1 rounded-3xl border-2 border-[#578AD6] bg-white text-sm bg-gray text-[#000]/[.6] font-bold">
-                    <div class="inline-block align-middle rounded-full border-1 w-[15px] h-[15px] mb-[3px] mr-2" :style="`background:${topic.color}`"></div>
+            <div class="flex gap-0 justify-end mr-[2vw]">
+                <div
+                    v-for="topic in data.topics"
+                    class="max-h-[80px] overflow-hidden break-all flex items-center px-4 py-1 mr-1 rounded-3xl border-2 border-[#578AD6] bg-white text-sm bg-gray text-[#000]/[.6] font-bold"
+                    style="flex-basis: 24%;"
+                >
+                    <div class="inline-block align-middle rounded-full border-1 w-[15px] min-w-[15px] h-[15px] min-h-[15px] mb-[3px] mr-2" :style="`background:${topic.color}`"></div>
                     {{ topic.topic }}
                 </div>
             </div>
