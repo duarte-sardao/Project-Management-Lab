f1bbb055671e792be4be9479c3c5433f07927648
fernandorego <fernandoluisrego@gmail.com>
2023-05-27 23:44:17 +0100

    Add message error if hospital already exists

--- lgp-sc17/resources/js/Components/MessageToast.vue
+++ lgp-sc17/resources/js/Components/MessageToast.vue
@@ -1,19 +1,19 @@
 <script setup>
-defineProps({
+const props = defineProps({
     message: String,
     error: String
 });
 </script>
 
 <template>
-    <div v-if="message !== 'null'">
+    <div v-if="message !== 'null' && message != null && message !== ''">
         <div class="toast toast-end" style="z-index: 99">
             <div class="alert alert-success text-white">
                 <span>{{ message }}</span>
             </div>
         </div>
     </div>
-    <div v-else-if="error !== 'null'">
+    <div v-else-if="error !== 'null' && error != null && error !== ''">
         <div class="toast toast-end" style="z-index: 99">
             <div class="alert alert-error text-white">
                 <span>{{ error }}</span>
